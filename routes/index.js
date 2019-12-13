fc614a097f9f9ee3083968f8cc3bcc485e9e1643
xluo14 <xluo14@stevens.edu>
2019-12-13 18:00:21 -0500

    perfume root

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,13 @@
 const perfumeRoutes = require("./perfumes");
 const userRoutes = require("./users");
+const homeRoutes = require("./root");
 const path = require("path");
 
 const constructorMethod = app => {
   app.use("/perfume", perfumeRoutes);
 
   app.use("/users", userRoutes);
-
-  app.get("/", (req,res)=>{
-    res.render('page/homePage');
-  });
+  app.use("/", homeRoutes);
 
   app.use("*", (req, res) => {
     res.status(404).render("page/errorPage", {errorMessage: "Page Not Found!"});
