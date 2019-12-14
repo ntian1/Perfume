8970edb670ecb3503cc5faca7063eb3aba115a54
Wangbomiao <44843493+Wangbomiao@users.noreply.github.com>
2019-12-14 17:00:46 -0500

    7

--- routes/root.js
+++ routes/root.js
@@ -5,11 +5,15 @@ const perfumeData = data.Perfume;
 const userData = data.users;
 
 router.get("/", async (req, res) => {
+    var authenticate=false;
+    if (req.session.cookie.expires!=false && req.session.cookie.expires!=null){
+      authenticate=true;
+    }
     try {
       res.render('page/homepage',{title:"home"});
 
     } catch (e) {
-        res.status(500).render("/page/errorPage",{ error: e });
+        res.status(500).render("/page/errorPage",{ error: e, authenticated:authenticate});
     }
   });  
 
