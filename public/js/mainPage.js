7ab4acc4cfbd2b4897fbe5586e25b2cc38299154
jsszrxy <jsszrxy@users.noreply.github.com>
2019-12-14 05:09:29 -0500

    12/14

--- public/js/mainPage.js
+++ public/js/mainPage.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
+    let link = window.location.href.replace("http://localhost:3000/", "");
+    if (link != "") {
+        link.replace("/", "+");
+        link = "search?q=" + link;
+    }
+    $("#facebookLink").prop("href", "http://www.facebook.com/share.php?u=https://www.google.com/" + link);
+    $("#fbshareLink").prop("href", "http://www.facebook.com/share.php?u=https://www.google.com/" + link);
+    $("#twitterLink", "#twshareLink").prop("href", "https://twitter.com/share?url=" + encodeURIComponent(window.location.href) + "&text=" + document.title, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
+
+
     //将电话加入剪切板
     $("#phoneLink").click(async function (event) {
         event.preventDefault();
@@ -28,7 +38,7 @@ $(document).ready(function () {
         if (event.which == 13 || event.keyCode == 13) {
             event.preventDefault();
             const searchContent = document.getElementById("searchInput").value;
-            window.location.href = "/perfume/search/" + searchContent;
+            window.location.href = "/search/" + searchContent;
             return false;
         }
         return true;
@@ -40,13 +50,149 @@ $(document).ready(function () {
         return false;
     });
 
-    let link = window.location.href.replace("http://localhost:3000/", "");
-    if (link != "") {
-        link.replace("/", "+");
-        link = "search?q=" + link;
-    }
-    $("#facebookLink").prop("href", "http://www.facebook.com/share.php?u=https://www.google.com/" + link);
-    $("#fbshareLink").prop("href", "http://www.facebook.com/share.php?u=https://www.google.com/" + link);
-    $("#twitterLink", "#twshareLink").prop("href", "https://twitter.com/share?url=" + encodeURIComponent(window.location.href) + "&text=" + document.title, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
+    $("#changeUserName").click(function (event){
+        event.preventDefault();
+        const p = $(this).parent();
+        p.find("ptag").addClass("hide");
+        p.find("#changeUserName").addClass("hide");
+        p.find("#userNameCancel").removeClass("hide");
+        $("#userNameForm").removeClass("hide");
+        return false;
+    });
 
+    $("#userNameCancel").click(function (event){
+        event.preventDefault();
+        const p = $(this).parent();
+        p.find("ptag").removeClass("hide");
+        p.find("#changeUserName").removeClass("hide");
+        p.find("#userNameCancel").addClass("hide");
+        $("#userNameForm").addClass("hide");
+        return false;
+    });
+
+    $("#changeEmailAddress").click(function (event){
+        event.preventDefault();
+        const p = $(this).parent();
+        p.find("ptag").addClass("hide");
+        p.find("#changeEmailAddress").addClass("hide");
+        p.find("#emailAddressCancel").removeClass("hide");
+        $("#emailAddressForm").removeClass("hide");
+        return false;
+    });
+
+    $("#emailAddressCancel").click(function (event){
+        event.preventDefault();
+        const p = $(this).parent();
+        p.find("ptag").removeClass("hide");
+        p.find("#changeEmailAddress").removeClass("hide");
+        p.find("#emailAddressCancel").addClass("hide");
+        $("#emailAddressForm").addClass("hide");
+        return false;
+    });
+
+    $("#changePassword").click(function (event){
+        event.preventDefault();
+        const p = $(this).parent();
+        p.find("ptag").addClass("hide");
+        p.find("#changePassword").addClass("hide");
+        p.find("#passwordCancel").removeClass("hide");
+        $("#passwordForm").removeClass("hide");
+        return false;
+    });
+
+    $("#passwordCancel").click(function (event){
+        event.preventDefault();
+        const p = $(this).parent();
+        p.find("ptag").removeClass("hide");
+        p.find("#changePassword").removeClass("hide");
+        p.find("#passwordCancel").addClass("hide");
+        $("#passwordForm").addClass("hide");
+        return false;
+    });
+
+    $("button").click(function (event){
+        event.preventDefault();
+        const p = $(this).parent();
+        if (!p.find("input")[0].value){
+            alert("Please input something!")
+            return false;
+        }
+        if (p[0].id == "userNameForm"){
+            $.ajax({
+                type: "POST",
+                url: window.location.href,
+                data: {
+                    userId: p.parent()[0].id,
+                    newUserName: p.find("#userName").value
+                },
+                success: function(result) {
+                    alert('Succes!');
+                    window.location.reload();
+                },
+                error: function(result) {
+                    alert("Error " + result);
+                }
+            });
+        }
+        if (p[0].id == "emailAddressForm"){
+            $.ajax({
+                type: "POST",
+                url: window.location.href,
+                data: {
+                    userId: p.parent()[0].id,
+                    newEmailAddress: p.find("#emailAddress").value
+                },
+                success: function(result) {
+                    alert('Succes!');
+                    window.location.reload();
+                },
+                error: function(result) {
+                    alert("Error " + result);
+                }
+            });
+        }
+        if (p[0].id == "passwordForm"){
+            $.ajax({
+                type: "POST",
+                url: window.location.href,
+                data: {
+                    userId: p.parent()[0].id,
+                    newPassword: p.find("#emailAddress").value
+                },
+                success: function(result) {
+                    alert('Succes!');
+                    window.location.reload();
+                },
+                error: function(result) {
+                    alert("Error " + result);
+                }
+            });
+        }
+        return false;
+    });
+
+    $(".changeReview").click(function (event){
+        event.preventDefault();
+        const p = $(this).parent();
+        const asddd = p.find(".reviewContent");
+        p.find(".reviewContent").addClass("hide");
+        p.find(".reviewRating").addClass("hide");
+        p.find(".changeReview").addClass("hide");
+        p.find(".deleteReview").addClass("hide");
+        p.find(".reviewCancel").removeClass("hide");
+        p.find(".perfumeReviewForm").removeClass("hide");
+        return false;
+    });
+
+    $(".reviewCancel").click(function (event){
+        event.preventDefault();
+        const p = $(this).parent();
+        p.find(".reviewContent").removeClass("hide");
+        p.find(".reviewRating").removeClass("hide");
+        p.find(".changeReview").removeClass("hide");
+        p.find(".deleteReview").removeClass("hide");
+        p.find(".reviewCancel").addClass("hide");
+        p.find(".perfumeReviewForm").addClass("hide");
+        return false;
+    });
 });
--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,15 @@ router.get("/login", (req, res) => {
     res.render("./page/loginPage", { title: "User", accountHidden: "hidden" });
 });
 
-router.post("/", (req, res) => {
-    res.status(500).render("./page/errorPage", { title: "user", errorMessage: "post user" });
+router.post("/:id", (req, res) => {
+    console.log(req.body.userId);
+    res.status(200).render("./page/errorPage", { title: "user", errorMessage: "post user" });
 });
 
 router.get("/:id", (req, res) => {
-    res.status(500).render("./page/errorPage", { title: "user", errorMessage: "get user id" });
+    res.status(500).render("./page/userPage", { title: "user", loginHidden:"hide", userName:"rxy", userId:"id", emailAddress: "asd@gmail.com", userReviews:[{perfumeId: "id", perfumeName: "Flower", reviewContent: "It is perfect!"}, {perfumeId: "id", perfumeName: "Grass", reviewContent: "It is not perfect!"}]});
 });
+
+
 
 module.exports = router;
