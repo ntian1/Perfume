d9c6a54584fa4ffadde315ad8027291bc6e5ef1f
xluo14 <xluo14@stevens.edu>
2019-12-14 23:35:39 -0500

    2

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const perfumeRoutes = require("./perfumes");
 const userRoutes = require("./users");
 const homeRoutes = require("./root");
+const searchRoutes = require("./search")
 const path = require("path");
 
 const constructorMethod = app => {
@@ -8,7 +9,7 @@ const constructorMethod = app => {
 
   app.use("/users", userRoutes);
   app.use("/", homeRoutes);
-
+  app.use("/search", searchRoutes);
   app.use("*", (req, res) => {
     res.status(404).render("page/errorPage", {errorMessage: "Page Not Found!"});
   });
